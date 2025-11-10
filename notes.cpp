5733c9ba68e26e0e4205d2d9432f6c52ec4219df
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-11-10 20:37:21 +0100

    del: Delete unneccessary comment

--- notes.cpp
+++ notes.cpp
@@ -1,17 +1,3 @@
-/* 
-
-C++ is still used today, significantly faster than python and java
-You need to learn the C++ standard library 
--> the standard library is huge though
--> The course entails plainty of exercises!
-
-What tools do you need to write in C++?
--> Integrated Developer Environment (MS Visual Studio)
--> XCode (Mac)
--> CLion (payed)
-
-*/
-
 #include <iostream>
 
 int main() {
