9c9c971857813167fd63f8632358c1d2527bb7d4
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-11-10 20:42:17 +0100

    feat: add notes about the continuation of the course

--- notes.cpp
+++ notes.cpp
@@ -4,3 +4,13 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
+/* To run a code, it has to be translated into machine code!*/
+/*If you want to run the code on another machine, you have to recompile it*/
+/*
+Basics: Fundamentals of programmings in C++
+Data Types
+Loops
+Functions
+*/
