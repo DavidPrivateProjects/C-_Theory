634b8ae0fc4dfcd8a8746aa234890aed7924c7a7
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-11-24 07:08:45 +0100

    feat: Calculate circumference of a circle

--- notes.cpp
+++ notes.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 int main(){
-    
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference << "cm";
 
     return 0;
 }
-
-// this video is very good, continue here tomorrow!
