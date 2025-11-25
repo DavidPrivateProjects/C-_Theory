fba852fb6b54e3d9392b8f20de782864054332ba
David <david.petrovic@akane.waseda.jp>
2025-11-25 21:11:13 +0100

    notes on namespaces

--- notes.cpp
+++ notes.cpp
@@ -2,7 +2,7 @@
 
 int main(){
     // Namespace allows for identicly named entities as long as the name spaces are different
-
+    // google why and how this works again, I don't fully understand
     int x = 0;
     int x = 1;
 
