3994ddb7d32cbc67ef928f12363a8c585e66dfc6
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-11-29 15:13:16 +0100

    feat: learn about using keyword

--- notes.cpp
+++ notes.cpp
@@ -4,11 +4,15 @@
 // new identifier usually ends with _t.
 typedef std::vector<std::pair<std::string, int>> pairlist_t;
 typedef std::string text_t;
+// typedef was replaced with the using keyword
+using text_t = std::string;
+using number_t = int;
 
 int main(){
 
     pairlist_t pairlist;
     text_t firstName = "Bro";
     std::cout << firstName << '\n';
+    std::cout << age << std::endl;
     return 0;
 }
