1864f5d87700b791ca598470f50ec96f3500dbc3
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-04 06:07:15 +0100

    feat: Test modulus operator

--- notes.cpp
+++ notes.cpp
@@ -4,19 +4,9 @@ int main()
 {
     int students = 20;
 
-    students = students + 1;
-    students+=1;
-    students++;
-    students = students-1;
-    students-=1;
-    students--;
-    students = students * 2;
-    students*=2;
-    students = students / 2;
-    students/=2;
-    students/=3; // decimal portion is lost.
+    int remainder = students % 3;
 
-    std::cout << students;
+    std::cout << remainder;
 
     return 0;
 }
