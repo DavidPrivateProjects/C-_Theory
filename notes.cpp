62cf2e6b8e4e923723be1a1b040d85d09d7ddd17
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-05 07:23:52 +0100

    feat: Use the getline function to receive an input

--- notes.cpp
+++ notes.cpp
@@ -2,12 +2,17 @@
 
 int main()
 {
-
     std::string name;
-    std::cout << "What's your name?: ";
-    std::cin >> name;
+    int age;
+    
+    std::cout << "What's your age?: ",
+    std::cin >> age;
 
-    std::cout << "Hello " << name;
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin >> std::ws, name);
+    
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
 
     return 0;
 }
