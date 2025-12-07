cbfd4c8d37fc5c5d48237d99234fea2fa466f77f
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-07 08:13:17 +0100

    feat: Implement the hypteghoras theorem in C++

--- notes.cpp
+++ notes.cpp
@@ -3,12 +3,20 @@
 
 int main()
 {
-    double x = 3;
-    double y = 4;
-    double z;
+    double a;
+    double b;
+    double c;
 
-    z = abs(-3);
-    z = round(x);
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "side C: " << c;
 
     return 0;
 }
