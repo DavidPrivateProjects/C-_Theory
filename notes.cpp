0a139cb062b56aa2013561548c5bd4465deba7e4
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-07 08:32:04 +0100

    feat: Further adapt the age restriction application

--- notes.cpp
+++ notes.cpp
@@ -11,6 +11,12 @@ int main()
     if(age >= 18){
         std::cout << "You are allowed to enter!" << std::endl;
     }
+    else if(age < 0){
+        std::cout << "You haven't been born yet!";
+    }
+    else if(age >= 100){
+        std::cout << "You are too old to enter this site!";
+    }
     else {
         std::cout << "You are not allowed to enter!" << std::endl;
     }
