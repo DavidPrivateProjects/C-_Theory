e453a72eaa44862a7e961837a124e8ea8d0d3b9b
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-07 08:14:01 +0100

    feat: Implement a better version of the code

--- notes.cpp
+++ notes.cpp
@@ -12,9 +12,7 @@ int main()
     std::cout << "Enter side B: ";
     std::cin >> b;
 
-    a = pow(a, 2);
-    b = pow(b, 2);
-    c = sqrt(a + b);
+    c = sqrt(pow(a, 2) + pow(b, 2));
 
     std::cout << "side C: " << c;
 
