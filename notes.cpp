1fd5b676754be06182a3ea94f823f724704ed618
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-08 06:56:53 +0100

    feat: Learn switch statements

--- notes.cpp
+++ notes.cpp
@@ -3,22 +3,20 @@
 
 int main()
 {
-    int age;
+    int month;
+    std::cout << "Enter the month (1-12): ";
+    std::cin >> month;
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-
-    if(age >= 18){
-        std::cout << "You are allowed to enter!" << std::endl;
-    }
-    else if(age < 0){
-        std::cout << "You haven't been born yet!";
-    }
-    else if(age >= 100){
-        std::cout << "You are too old to enter this site!";
-    }
-    else {
-        std::cout << "You are not allowed to enter!" << std::endl;
+    switch(month){
+        case 1: 
+            std::cout << "It is January";
+            break;
+        case 2:
+            std::cout << "It is February";
+            break;
+        default:
+            std::cout << "I don't know what month it is!";
+            break;
     }
 
     return 0;
