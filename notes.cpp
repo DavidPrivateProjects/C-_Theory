97a6766e3906f0e69625e22f51d9c7379119ab32
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-09 04:56:32 +0100

    feat: Implement further examples for tenery operator

--- notes.cpp
+++ notes.cpp
@@ -3,8 +3,14 @@
 
 int main()
 {
-    int grade = 75;
-    grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail!";
     
+    int number = 9;
+    number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
+
+    bool hungry = true;
+    hungry ? std::cout << "You are hungry" : std::cout << "You are full" << std::endl;
+
+    std::cout << (hungry ? "You are hungry" : "You are full");
+
     return 0;
 }
