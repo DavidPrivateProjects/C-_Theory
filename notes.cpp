987bbda0fd0a00c57e30bc888efc6ced76bff5b0
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-09 04:53:46 +0100

    feat: Implement tenery operator

--- notes.cpp
+++ notes.cpp
@@ -3,40 +3,8 @@
 
 int main()
 {
-    char op;
-    double num1;
-    double num2;
-    double result;
-
-    std::cout << "***** CALCULATOR *****" << std::endl;
-
-    std::cout << "Enter either (+ - * /): ";
-    std::cin >> op;
-
-    std::cout << "Enter #1: ";
-    std::cin >> num1;
-
-    std::cout << "Enter #2: ";
-    std::cin >> num2;
-
-    switch(op){
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        default:
-            std::cout << "That wasn't a valid response" << std::endl;
-    }
-
-    std::cout << "**********************";
-
+    int grade = 75;
+    grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail!";
+    
     return 0;
 }
