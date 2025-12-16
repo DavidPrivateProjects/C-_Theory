fa3c7551b6cda306828f32916d3061364ac391d1
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-16 07:33:14 +0100

    feat: Happy new year!

--- notes.cpp
+++ notes.cpp
@@ -6,14 +6,12 @@ int main()
     
     // do while loop
 
-    int number;
-
-    do{
-        std::cout << "Enter a positive #: ";
-        std::cin >> number;
-    } while (number < 0);
-
-    std::cout << "The # is: " << number;
+    int main()
+    {
+        for(int i = 1; i <= 3; i++){
+            std::cout << "HAPPY NEW YEAR!";
+        }
+    }
 
     return 0;
 }
