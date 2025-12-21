dd6f6cdcf2b391b8ed7e62bcadf69d0c81b5f129
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-12-21 09:40:44 +0100

    feat: Add an argument to the functions

--- notes.cpp
+++ notes.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 #include <ctime>
 
-void happyBirthday();
+void happyBirthday(std::string name);
 
 int main()
 {
     
     // function = a block of reusable code
 
-    happyBirthday();
-    happyBirthday();
-    happyBirthday();
+    happyBirthday("Bro");
+    happyBirthday("Bro");
+    happyBirthday("Bro");
 
     return 0;
 }
 
 
-void happyBirthday(){
-    std::cout << "Happy Birthday to you!\n";
+void happyBirthday(std::string name){
+    std::cout << "Happy Birthday to you!" << name << "\n";
 }
