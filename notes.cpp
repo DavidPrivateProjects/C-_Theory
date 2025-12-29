97c95fc92dcbc226ec62c14352476d8f5ffc762e
David <david.petrovic@akane.waseda.jp>
2025-12-29 08:21:18 +0100

    feat: Create variables with different scopes

--- notes.cpp
+++ notes.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
-void bakePizza();
-void bakePizza(std::string topping1);
 
 int main()
 {
-    bakePizza();
-    bakePizza("Pepperoni");
+    // Local variables 
+    // Global variables
+
+    int myNum = 1;
+    std::cout << myNum;
+
+    printNum()
+
     return 0;
 }
 
-void bakePizza(){
-    std::cout << "Here is your pizza!\n";
-}
-void bakePizza(std::string topping1){
-    std::cout << "Here is your " << topping1 << " pizza\n";
+
+void printNum(){
+    int myNum = 2;
+    std::cout << myNum;
 }
