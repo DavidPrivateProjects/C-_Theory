29ed207455129fa2871b411ff821e41ec4138da4
David <david.petrovic@akane.waseda.jp>
2026-01-03 10:09:51 +0100

    feat: Create backbone of game

--- notes.cpp
+++ notes.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
+
 int main()
 {
 
     return 0;
+}
+
+
+char getUserChoice(){
+    return 0;
+}
+char getComputerChoice(){
+    return 0;
+}
+void showChoice(char choice){
+
+}
+void chooseWinner(char player, char computer){
+
 }
 
