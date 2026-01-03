507a3bb82838154d18aeb1a7e6ddf942cafcbb7e
David <david.petrovic@akane.waseda.jp>
2026-01-03 10:13:14 +0100

    feat: Finish getUserChoice function

--- notes.cpp
+++ notes.cpp
@@ -7,12 +7,24 @@ void chooseWinner(char player, char computer);
 
 int main()
 {
+    char player;
+    char computer;
+
+    player = getUserChoice()
 
     return 0;
 }
 
 
 char getUserChoice(){
+    char player;
+    
+    std::cout << "Rock-Paper-Scissors Game!\n";
+    std::cout << "r' for rock\n";
+    std::cout << "p' for paper\n";
+    std::cout << "s' for scissors\n";
+
+    std::cin >> player;
     return 0;
 }
 char getComputerChoice(){
