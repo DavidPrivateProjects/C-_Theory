98737ac79ea69797b6677cd9f1bf025cf9e8f45c
David <david.petrovic@akane.waseda.jp>
2026-01-14 17:37:51 +0100

    feat: Create array without elements

--- notes.cpp
+++ notes.cpp
@@ -2,9 +2,16 @@
 
 int main(){
 
+    // Can only contain data of the same data type!!!
     std::string car[] = {"Corvette", "Mustang", "Camry"};
 
+    std::string car2[3]; // Static data structure needs a size!!!
+
     std::cout << car[0];
+    std::cout << car[1];
+    std::cout << car[2];
+
+    car2[0] = "Hey"
 
     return 0;
 
