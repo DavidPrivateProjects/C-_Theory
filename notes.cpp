f894607fbe89e81a666f0f262c1d40e299713961
David <david.petrovic@akane.waseda.jp>
2026-01-20 05:46:01 +0100

    feat: Check size of different variables

--- notes.cpp
+++ notes.cpp
@@ -4,8 +4,12 @@ int main(){
 
     // determines the size in bytes of an object
     double gpa = 2.5;
+    std::string name = "Bro Code";
+    char grade = "F";
 
     std::cout << sizeof(gpa) << " bytes\n";
+    std::cout << sizeof(name) << " bytes\n";
+    std::cout << sizeof(grade) << " bytes\n";
 
     return 0;
 
